f46ffff78dd6b083d71b1533c2868673e6719904
boB Rudis <bob@rud.is>
2019-01-16 03:03:23 -0500

    package doc

--- R/iceout-package.R
+++ R/iceout-package.R
@@ -1,12 +1,18 @@
 #' Retrieve and Process 'Ice-out' Data for New England and Minnesota Inland Bodies of Water
 #'
-#' @seealso <https://me.water.usgs.gov/iceout_data>
-#' @description Lake ice-out dates, or the dates of ice break-up, are the annual dates in spring
+#' \if{html}{
+#' \figure{iceout-logo.png}{options: width="25\%" valign="top" align="right" style="padding-left:10px" alt="Figurre: iceout logo"}
+#' }
+#' \if{latex}{
+#' \figure{iceout-logo.png}{options: width=10cm}
+#' }
+#' Lake ice-out dates, or the dates of ice break-up, are the annual dates in spring
 #' when winter ice cover leaves a lake. Methods are provides to enable convenient access to
-#' New England iceout data from
-#' <https://me.water.usgs.gov/iceout_data> and Minnesota iceout data from
-#' <https://www.dnr.state.mn.us/ice_out/index.html?year=1843> along with
-#' extra Maine state data from <https://www.maine.gov/dacf/parks/water_activities/boating/ice_out06.shtml>.
+#' New England iceout data from the [USGS](https://me.water.usgs.gov/iceout_data>) and Minnesota iceout data from
+#' the [MDNR](https://www.dnr.state.mn.us/ice_out/index.html?year=1843) along with
+#' extra Maine state data from [MDACF](https://www.maine.gov/dacf/parks/water_activities/boating/ice_out06.shtml).
+#'
+#' @seealso <https://me.water.usgs.gov/iceout_data>
 #' @name iceout
 #' @aliases umoos iceout-package
 #' @importFrom magrittr %>%
